7d37fc79f6a82aff60d98fdf6165a819c96c18b6
Chris Mavrakis <cmavr8@gmail.com>
2020-04-18 12:59:03 +0200

    Add encryption. This should reduce errors from 4 to 3. Build should still fail

--- iam_students.tf
+++ iam_students.tf
@@ -117,4 +117,12 @@ output "passwords" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "dummybucket"
   acl = "private"
+
+ server_side_encryption_configuration {
+   rule {
+     apply_server_side_encryption_by_default {
+       sse_algorithm = "AES256"
+     }
+   }
+ }
 }

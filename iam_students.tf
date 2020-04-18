7804d85b32607800034469d4549e4b19a69d97fb
Chris Mavrakis <cmavr8@gmail.com>
2020-04-18 14:41:00 +0200

    Remove all security properties. Checkov should be up in arms

--- iam_students.tf
+++ iam_students.tf
@@ -117,20 +117,4 @@ output "passwords" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "dummybucket"
   acl = "private"
-
- server_side_encryption_configuration {
-   rule {
-     apply_server_side_encryption_by_default {
-       sse_algorithm = "AES256"
-     }
-   }
- }
-
-  versioning {
-    enabled = true
-  }
-
-  logging {
-    target_bucket = "a-nonexistent-bucket"
-  }
 }
